00d18c28fb1b90a4c1d4d8a81f97e7be064eee5a
John Paul Del Mundo <81340844+jpdelmundo223@users.noreply.github.com>
2022-02-07 11:42:39 +0800

    Delete __init__.py

--- easy/__init__.py
+++
@@ -1,8 +0,0 @@
-from app import duplicate_encode
-from app import duplicate_encode_refactored
-text = "The quick brown fox jumps over the lazy dog."
-
-if __name__ == "__main__":
-    # runs the py application
-    print(duplicate_encode(text))
-    print(duplicate_encode_refactored(text))
