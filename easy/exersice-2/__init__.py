94c906e0563a25692b1bad554674295fdf5cbecc
John Paul Del Mundo <john.delmundo@CEGROUP.com>
2022-02-14 13:17:02 +0800

    added some notes

--- easy/exersice-2/__init__.py
+++ easy/exersice-2/__init__.py
@@ -1,4 +1,10 @@
 from app import alphabet_position
 
 if __name__ == "__main__":
+    """if __name__ == “main”: is used to execute some code only if the file was run 
+    directly, and not imported.
+
+    Some useful links:
+    - https://www.freecodecamp.org/news/if-name-main-python-example/
+    - https://codefather.tech/blog/if-name-main-python/"""
     print(alphabet_position("transfer"))
