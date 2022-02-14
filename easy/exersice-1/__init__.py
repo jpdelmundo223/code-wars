5f6d874b90558873c57b2ac93dd5a788d5957ced
John Paul Del Mundo <john.delmundo@CEGROUP.com>
2022-02-14 13:16:42 +0800

    added some notes

--- easy/exersice-1/__init__.py
+++ easy/exersice-1/__init__.py
@@ -3,6 +3,11 @@ from app import duplicate_encode_refactored
 text = "The quick brown fox jumps over the lazy dog."
 
 if __name__ == "__main__":
-    # runs the py application
+    """if __name__ == “main”: is used to execute some code only if the file was run 
+    directly, and not imported.
+
+    Some useful links:
+    - https://www.freecodecamp.org/news/if-name-main-python-example/
+    - https://codefather.tech/blog/if-name-main-python/"""
     print(duplicate_encode(text))
     print(duplicate_encode_refactored(text))
